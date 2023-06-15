bddeb3e85364f2e4a65d618c3798e505e6fd60e5
Alexander Artamonov <artamonov@newton-technology.ru>
2023-06-15 21:30:30 +0300

    Поправил имена переменных согласно домашнему заданию

--- L12_Worker/src/Main.java
+++ L12_Worker/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Worker worker = new Worker(System.out::println, System.out::println);
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener, errorListener);
         worker.start();
     }
 }
--- L12_Worker/src/Worker.java
+++ L12_Worker/src/Worker.java
@@ -1,19 +1,19 @@
 public class Worker {
-    OnTaskDoneListener onTaskDoneListener;
-    OnTaskErrorListener onTaskErrorListener;
+    OnTaskDoneListener callback;
+    OnTaskErrorListener errorCallback;
 
-    public Worker(OnTaskDoneListener onTaskDoneListener, OnTaskErrorListener onTaskErrorListener) {
-        this.onTaskDoneListener = onTaskDoneListener;
-        this.onTaskErrorListener = onTaskErrorListener;
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
     }
 
     public void start() {
         for (int i = 1; i < 101; i++) {
             if (i == 33) {
-                onTaskErrorListener.onError("Task " + i + " has errors");
+                errorCallback.onError("Task " + i + " has errors");
                 continue;
             }
-            onTaskDoneListener.onDone("Task " + i + " is done");
+            callback.onDone("Task " + i + " is done");
         }
     }
 }
