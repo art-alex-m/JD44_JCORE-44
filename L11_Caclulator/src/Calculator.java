49a9ff6a3c4c4b6fb8a39e8b33615be693ef1a1c
Alexander Artamonov <artamonov@newton-technology.ru>
2023-06-19 11:18:59 +0300

    L1 Lambda func: Задача 1.1 Калькулятор. Внес правки

--- L11_Caclulator/src/Calculator.java
+++ L11_Caclulator/src/Calculator.java
@@ -6,7 +6,13 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y == 0) {
+            System.out.println("Деление на ноль!");
+            return Integer.MIN_VALUE;
+        }
+        return x / y;
+    };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
--- L11_Caclulator/src/Main.java
+++ L11_Caclulator/src/Main.java
@@ -2,17 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
 
-        int a = calc.plus.apply(1,2);
-        int b = calc.minus.apply(1,1);
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
 
         // Код не работает, потому что происходит деление на 0
         // Можно обернуть в try/catch и отлавливать ArithmeticException.
         // Можно сделать предварительную, проверку, что b не равно нулю. Этот вариант работает быстрее
-        if (b != 0) {
-            int c = calc.devide.apply(a, b);
-            calc.println.accept(c);
-        } else {
-            System.out.println("Деление на ноль!");
-        }
+        int c = calc.devide.apply(a, b);
+        calc.println.accept(c);
     }
 }
