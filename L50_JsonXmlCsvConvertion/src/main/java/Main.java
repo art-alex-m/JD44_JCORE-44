6ee9e00c700aece70dbddede0af73d493fb40373
Alexander Artamonov <artamonov@newton-technology.ru>
2023-07-30 19:52:59 +0300

    L5 JsonXmlCsv: Задача 2. XML - JSON парсер

--- L50_JsonXmlCsvConvertion/src/main/java/Main.java
+++ L50_JsonXmlCsvConvertion/src/main/java/Main.java
@@ -4,10 +4,19 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -16,29 +25,25 @@ public class Main {
         /// 1. CSV -> JSON
         String[] positionColumnCsvMap = {"id", "firstName", "lastName", "country", "age"};
         employeeList = parseCsv("data.csv", positionColumnCsvMap);
-        writeJsonToFile(employeeList, "data.json");
+        createAndWriteJsonToFile(employeeList, "data.json");
 
         /// 2. XML -> JSON
         employeeList = parseXml("data.xml");
-        writeJsonToFile(employeeList, "data2.json");
+        createAndWriteJsonToFile(employeeList, "data2.json");
     }
 
-    private static List<Employee> parseXml(String inputXmlFileName) {
-        return null;
-    }
-
-    private static void writeJsonToFile(List<Employee> employeeList, String outJsonFileName) {
+    private static void createAndWriteJsonToFile(List<Employee> employeeList, String outJsonFileName) {
 
         if (employeeList == null) {
-            System.out.println("Empty result of parsing csv file");
+            System.out.println("Empty result of parsing resource file");
             return;
         }
         String employeeJson = listToJson(employeeList);
         System.out.println(employeeJson);
         if (writeString(outJsonFileName, employeeJson)) {
-            System.out.println("Write employee list to data.json successful");
+            System.out.printf("Write employee list to %s successful%n", outJsonFileName);
         } else {
-            System.out.println("CSV Json write error");
+            System.out.println("Json write error");
         }
     }
 
@@ -74,5 +79,45 @@ public class Main {
         }
 
         return null;
+    }
+
+    private static List<Employee> parseXml(String inputXmlFileName) {
+        try {
+            DocumentBuilder xmlBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = xmlBuilder.parse(inputXmlFileName);
+
+            NodeList staffList = doc.getElementsByTagName("employee");
+            List<Employee> employeeList = new ArrayList<>(8);
+            for (int i = 0; i < staffList.getLength(); i++) {
+                Employee employee = createEmployeeFromXmlNode(staffList.item(i));
+                employeeList.add(employee);
+            }
+
+            return employeeList;
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+
+    private static Employee createEmployeeFromXmlNode(Node node) {
+        Employee employee = new Employee();
+        NodeList fieldsList = node.getChildNodes();
+        for (int j = 0; j < fieldsList.getLength(); j++) {
+            Node fieldNode = fieldsList.item(j);
+            if (fieldNode.getNodeType() == Element.ELEMENT_NODE) {
+                Element element = (Element) fieldNode;
+                String context = element.getTextContent();
+                switch (element.getTagName()) {
+                    case "id" -> employee.setId(Long.parseLong(context));
+                    case "firstName" -> employee.setFirstName(context);
+                    case "lastName" -> employee.setLastName(context);
+                    case "country" -> employee.setCountry(context);
+                    case "age" -> employee.setAge(Integer.parseInt(context));
+                }
+            }
+        }
+        return employee;
     }
 }
